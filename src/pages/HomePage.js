7f150aafd3b88e05277987dc4952846bd081171f
kimdooh <johnkdo2020@gmail.com>
2021-12-24 17:50:59 +0900

    top header 없애봄 임시로

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,14 +3,14 @@ import ChannelTemplatContainer from '../containers/ChannelTemplateContainer';
 import Header from '../containers/Header';
 
 const HomePage = ({ match }) => {
-  const category = match.params.category || 'searchChannelRank';
-  return (
-    <div>
-      <Header />
-      <ChannelTemplatContainer category={category} />
-      {/* 동영상 순위 썸네일 포함 */}
-      {/* <VideoTemplateContainer /> */}
-    </div>
-  );
+    const category = match.params.category || 'searchChannelRank';
+    return (
+        <div>
+            {/* <Header /> */}
+            <ChannelTemplatContainer category={category} />
+            {/* 동영상 순위 썸네일 포함 */}
+            {/* <VideoTemplateContainer /> */}
+        </div>
+    );
 };
 export default HomePage;
